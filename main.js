878e0c1dbe918197c24d192d8d9cddf25a35167c
Julia Prano <juliiprano@gmail.com>
2023-08-17 20:32:16 -0300

    prueba js

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
-alert('Bienvenido a tu cronograma diario en la web')
+alert('Bienvenido a Bebidas Carolinas, tu distribuidora favorita')
+
+const edad = <18
+let edadingresada parseFloat(prompt('Ingrese su edad'))
 
         let tareasdiariasxdia =(prompt('cuantas tareas quieres realizar hoy?'))
         // while (isNaN(tareasdiariasxdia)) {
         //     tareasdiariasxdia = parseFloat(prompt('eso no es un numero, por favor, ingresalo nuevamente'))
         // }
-        for (let cantdetaresyahechas = 0; cantdetarresQueYahice < tareasdiariasxdia; cantdetarresQueYahice = tareasdiariasxdia + 1) {
+        for (let cantdetareasyahechas = 0; cantdetareasQueYahice < tareasdiariasxdia; cantdetareasQueYahice = tareasdiariasxdia + 1) {
             alert('felicidades una tarea bien hecha!')
-            alert('le sumo una a las ' + cantdetaresyahechas + ' que ya hice (total: ' + (cantdetarresQueYahice + 1))
+            alert('le sumo una a las ' + cantdetareasyahechas + ' que ya hice (total: ' + (cantdetarresQueYahice + 1))
 
         }
