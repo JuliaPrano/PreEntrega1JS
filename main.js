76155ca0807a21de869cf0e4418c81b8a2960410
Julia Prano <juliiprano@gmail.com>
2023-08-19 20:45:47 -0300

    modificaciones en js

--- main.js
+++ main.js
@@ -1,14 +1,35 @@
 alert('Bienvenido a Bebidas Carolinas, tu distribuidora favorita')
 
-const edad = <18
-let edadingresada parseFloat(prompt('Ingrese su edad'))
+let edad = parseInt(prompt('Ingrese su edad'));
+if (edad > 17) {
+    alert('Disfruten de nuestra web')
+}
+else {
+    alert('Lo sentimos no podras comprar en nuestra web')
+}
+let entrada = prompt("Desea ver precio de bebidas con o sin alcohol?");
+switch (entrada) {
+    case "con":
+        alert("Bebidas con alcohol a partir de $500");
+        break;
+    case "sin":
+        alert("Bebidas sin alcohol a partir de $500");
+        break;
+    default:
+        alert("Por favor, solo responda: con o sin")
+        break;
+}
 
-        let tareasdiariasxdia =(prompt('cuantas tareas quieres realizar hoy?'))
-        // while (isNaN(tareasdiariasxdia)) {
-        //     tareasdiariasxdia = parseFloat(prompt('eso no es un numero, por favor, ingresalo nuevamente'))
-        // }
-        for (let cantdetareasyahechas = 0; cantdetareasQueYahice < tareasdiariasxdia; cantdetareasQueYahice = tareasdiariasxdia + 1) {
-            alert('felicidades una tarea bien hecha!')
-            alert('le sumo una a las ' + cantdetareasyahechas + ' que ya hice (total: ' + (cantdetarresQueYahice + 1))
+// ------------------------------------
+function aplicarDescuento(precioAlcohol, porcentajeDescuento) {
+    const montoADescontar = precioAlcohol / 100 * porcentajeDescuento
+    const precioFinal = precioAlcohol - montoADescontar
+    return precioFinal
+  }
+//   ----------------------------------------
+const precioAlcohol = parseFloat(prompt('ingrese el monto inicial'))
+const porcentajeDescuento = parseFloat(prompt('ingrese el descuento a aplicar'))
 
-        }
+const resultado = aplicarDescuento(precioAlcohol, porcentajeDescuento)
+alert(resultado)
+alert('Gracias')
